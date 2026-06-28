21075e507ec84e9583f760ec170feea99bac28af
Gabriel Chierentin <120571930+gabriel-chierentin@users.noreply.github.com>
2026-06-28 12:20:55 -0300

    Initial commit

---
+++ src/main/java/com/postest/PostestApplication.java
@@ -0,0 +1,13 @@
+package com.postest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostestApplication.class, args);
+	}
+
+}
